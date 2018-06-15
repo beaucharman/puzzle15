bee3fd941ffb9618a6f4570847991bdf2c0934b0
Beau Charman <beau.charman@gmail.com>
2018-06-15 22:48:47 +1000

    game logic

--- src/App.js
+++ src/App.js
@@ -15,32 +15,76 @@ const initialState = () => ({
 class App extends Component {
   state = initialState()
 
+  swapItems = (arr, rowA, indexA, rowB, indexB) => {
+    const matrix = arr.concat([]) // so not to mutate the array refernce
+    const temp = matrix[rowA][indexA]
+    matrix[rowA][indexA] = matrix[rowB][indexB]
+    matrix[rowB][indexB] = temp
+    return matrix
+  }
 
+  findCoords = (matrix, target) => {
+    let result
+    let row = 0
+    matrix.forEach((list) => {
+      if (list.indexOf(target) > -1) {
+        result = { x: row, y: list.indexOf(target) }
+      }
+
+      row++
+    })
+    return result
+  }
+
+  checkCanMove = (targetCoords, blankCoords) => {
+    // by row
+    if (targetCoords.x === blankCoords.x) {
+      if (targetCoords.y === blankCoords.y - 1 || targetCoords.y === blankCoords.y + 1) {
+        return true
+      }
+    // by column
+    } else if (targetCoords.y === blankCoords.y) {
+      if (targetCoords.x === blankCoords.x - 1 || targetCoords.x === blankCoords.x + 1) {
+        return true
+      }
+    } else {
+      return false
+    }
+  }
 
   handleTileClick = (tileNumber) => {
     // 1. check that the target has a `number` not `0`
     if (tileNumber < 1) {
-      // 1.1 if false, do nothing
+      // 1.1. if false, do nothing
       return false
     }
 
-    console.log('Tile: ', tileNumber)
+    const { data } = this.state
 
+    // 1.2. find the coords of the clicked element
+    const targetCoords = this.findCoords(data, tileNumber)
 
-    // 2 if true, check for a blank tile next to the
-    //   clicked target tile
+    // 1.3. find the coords of the blank element
+    const blankCoords = this.findCoords(data, 0)
 
-      //  2.1 check that the position right of the
-      //      target or left of it has the blank tile
+    // 2. if true, check for a blank tile next to the
+    //    clicked target tile
+    //    2.1. check that the position right of the
+    //         target or left of it has the blank tile
+    //    2.2. check that the row above or below
+    //         + the same position has the blank tile
+    const canMove = this.checkCanMove(targetCoords, blankCoords)
 
-      // 2.2 check that the row above or below
-      //     + pos above has the blank tile
-
-      // 2.3 if not, do nothing
-
+    // 2.3. if not, do nothing
+    if (!canMove) {
+      return false
+    }
 
     // 3. if the target does have an adjacent blank tile,
     //    switch the tiles and update the state
+    this.setState(() => ({
+      data: this.swapItems(data, targetCoords.x, targetCoords.y, blankCoords.x, blankCoords.y),
+    }))
   }
 
   renderTileFromRow = (row) => row.map(
@@ -55,6 +99,8 @@ class App extends Component {
   )
 
   renderTiles = () => this.state.data.map(this.renderTileFromRow)
+
+  // @TODO ramdomise tiles in state on initial render
 
   render() {
     return (
